02e3e933c9dc842f5f91261640736dd50a1935d9
fushuwei <331044761@qq.com>
2023-01-18 11:37:02 +0800

    init project

---
+++ gwt-basic/src/main/java/com/mochousoft/gwt/basic/GwtBasicApplication.java
@@ -0,0 +1,13 @@
+package com.mochousoft.gwt.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwtBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwtBasicApplication.class, args);
+    }
+
+}
---
+++ gwt-basic/src/test/java/com/mochousoft/gwt/basic/GwtBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mochousoft.gwt.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwtBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
