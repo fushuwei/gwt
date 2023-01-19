a2fc6af23f4bc47d9b5e230bfc16443dbafa383e
fushuwei <331044761@qq.com>
2023-01-19 22:14:58 +0800

    修改模块依赖关系

---
+++ gwt-spring-boot/gwt-spring-boot-server/src/main/java/com/mochousoft/gwt/server/servlet/MessageServiceImpl.java
@@ -0,0 +1,22 @@
+package com.mochousoft.gwt.server.servlet;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.mochousoft.gwt.shared.MessageService;
+
+import java.time.LocalDateTime;
+
+/**
+ * The server-side implementation of the RPC service.
+ *
+ * @author fushuwei
+ */
+public class MessageServiceImpl extends RemoteServiceServlet implements MessageService {
+
+    public String sendMessage(String message) throws IllegalArgumentException {
+        if (message == null) {
+            throw new IllegalArgumentException("message is null");
+        }
+
+        return "Hello, " + message + "!<br><br> Time received: " + LocalDateTime.now();
+    }
+}
