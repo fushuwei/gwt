eed1779d128373f9bab6454225653059ee881542
fushuwei <331044761@qq.com>
2023-01-28 22:38:20 +0800

    解决webapp目录中index.html不生效问题

--- gwt-rest-springboot-basic/gwt-rest-springboot-basic-server/src/main/java/com/mochousoft/gwt/server/GWTApplication.java
+++ gwt-rest-springboot-basic/gwt-rest-springboot-basic-server/src/main/java/com/mochousoft/gwt/server/GWTApplication.java
@@ -2,12 +2,21 @@ package com.mochousoft.gwt.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GWTApplication {
+@ServletComponentScan
+public class GWTApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GWTApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GWTApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GWTApplication.class);
+    }
 
 }
