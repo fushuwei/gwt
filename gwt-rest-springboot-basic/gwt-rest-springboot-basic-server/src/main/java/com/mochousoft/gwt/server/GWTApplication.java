2d63108aca91699629e5883ab6283e10dda5b964
fushuwei <331044761@qq.com>
2023-01-28 22:48:50 +0800

    还原

--- gwt-rest-springboot-basic/gwt-rest-springboot-basic-server/src/main/java/com/mochousoft/gwt/server/GWTApplication.java
+++ gwt-rest-springboot-basic/gwt-rest-springboot-basic-server/src/main/java/com/mochousoft/gwt/server/GWTApplication.java
@@ -2,21 +2,14 @@ package com.mochousoft.gwt.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
-public class GWTApplication extends SpringBootServletInitializer {
+public class GWTApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GWTApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(GWTApplication.class);
     }
 
 }
