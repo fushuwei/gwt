d068321910cded5034ec3645e48a057497cd96d4
fushuwei <331044761@qq.com>
2023-02-13 22:07:11 +0800

    修改代码

--- gwt-samples/gwt-voice/src/main/java/com/mochousoft/gwt/client/App.java
+++ gwt-samples/gwt-voice/src/main/java/com/mochousoft/gwt/client/App.java
@@ -1,7 +1,6 @@
 package com.mochousoft.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
