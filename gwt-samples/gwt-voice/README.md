633f5a568e28edab1775b2b332e10007546ba784
fushuwei <331044761@qq.com>
2023-02-13 18:12:01 +0800

    修改demo

--- gwt-samples/gwt-voice/README.md
+++ gwt-samples/gwt-voice/README.md
@@ -1,9 +1,38 @@
-# Step 1. Install *gwt-jelement* to local Maven repository
+# Development
+
+## Step 1. Install *gwt-jelement* to local Maven repository
 
 ``git clone https://github.com/gwt-jelement/gwt-jelement.git``
 
 ``mvn install``
 
-# Step 2. Build and Run
+## Step 2. Build and Run
 
 ``mvn clean gwt:generate-module gwt:devmode``
+
+# Supported languages
+
+```shell
+0: SpeechSynthesisVoice {voiceURI: 'Microsoft Huihui - Chinese (Simplified, PRC)', name: 'Microsoft Huihui - Chinese (Simplified, PRC)', lang: 'zh-CN', localService: true, default: true}
+1: SpeechSynthesisVoice {voiceURI: 'Microsoft Kangkang - Chinese (Simplified, PRC)', name: 'Microsoft Kangkang - Chinese (Simplified, PRC)', lang: 'zh-CN', localService: true, default: false}
+2: SpeechSynthesisVoice {voiceURI: 'Microsoft Yaoyao - Chinese (Simplified, PRC)', name: 'Microsoft Yaoyao - Chinese (Simplified, PRC)', lang: 'zh-CN', localService: true, default: false}
+3: SpeechSynthesisVoice {voiceURI: 'Google Deutsch', name: 'Google Deutsch', lang: 'de-DE', localService: false, default: false}
+4: SpeechSynthesisVoice {voiceURI: 'Google US English', name: 'Google US English', lang: 'en-US', localService: false, default: false}
+5: SpeechSynthesisVoice {voiceURI: 'Google UK English Female', name: 'Google UK English Female', lang: 'en-GB', localService: false, default: false}
+6: SpeechSynthesisVoice {voiceURI: 'Google UK English Male', name: 'Google UK English Male', lang: 'en-GB', localService: false, default: false}
+7: SpeechSynthesisVoice {voiceURI: 'Google español', name: 'Google español', lang: 'es-ES', localService: false, default: false}
+8: SpeechSynthesisVoice {voiceURI: 'Google español de Estados Unidos', name: 'Google español de Estados Unidos', lang: 'es-US', localService: false, default: false}
+9: SpeechSynthesisVoice {voiceURI: 'Google français', name: 'Google français', lang: 'fr-FR', localService: false, default: false}
+10: SpeechSynthesisVoice {voiceURI: 'Google हिन्दी', name: 'Google हिन्दी', lang: 'hi-IN', localService: false, default: false}
+11: SpeechSynthesisVoice {voiceURI: 'Google Bahasa Indonesia', name: 'Google Bahasa Indonesia', lang: 'id-ID', localService: false, default: false}
+12: SpeechSynthesisVoice {voiceURI: 'Google italiano', name: 'Google italiano', lang: 'it-IT', localService: false, default: false}
+13: SpeechSynthesisVoice {voiceURI: 'Google 日本語', name: 'Google 日本語', lang: 'ja-JP', localService: false, default: false}
+14: SpeechSynthesisVoice {voiceURI: 'Google 한국의', name: 'Google 한국의', lang: 'ko-KR', localService: false, default: false}
+15: SpeechSynthesisVoice {voiceURI: 'Google Nederlands', name: 'Google Nederlands', lang: 'nl-NL', localService: false, default: false}
+16: SpeechSynthesisVoice {voiceURI: 'Google polski', name: 'Google polski', lang: 'pl-PL', localService: false, default: false}
+17: SpeechSynthesisVoice {voiceURI: 'Google português do Brasil', name: 'Google português do Brasil', lang: 'pt-BR', localService: false, default: false}
+18: SpeechSynthesisVoice {voiceURI: 'Google русский', name: 'Google русский', lang: 'ru-RU', localService: false, default: false}
+19: SpeechSynthesisVoice {voiceURI: 'Google 普通话（中国大陆）', name: 'Google 普通话（中国大陆）', lang: 'zh-CN', localService: false, default: false}
+20: SpeechSynthesisVoice {voiceURI: 'Google 粤語（香港）', name: 'Google 粤語（香港）', lang: 'zh-HK', localService: false, default: false}
+21: SpeechSynthesisVoice {voiceURI: 'Google 國語（臺灣）', name: 'Google 國語（臺灣）', lang: 'zh-TW', localService: false, default: false}
+```
--- gwt-samples/gwt-voice/src/main/java/com/mochousoft/gwt/client/App.java
+++ gwt-samples/gwt-voice/src/main/java/com/mochousoft/gwt/client/App.java
@@ -3,7 +3,10 @@ package com.mochousoft.gwt.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import gwt.jelement.core.Promise;
 import gwt.jelement.speech.SpeechSynthesisUtterance;
 import gwt.jelement.speech.SpeechSynthesisVoice;
@@ -27,25 +30,59 @@ public class App implements EntryPoint {
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        Button button = new Button("语音合成", (ClickHandler) clickEvent -> speechSynthesis());
-        RootPanel.get().add(button);
+        // 标题
+        HTML title = new HTML("<h1>语音合成示例</h1>");
+
+        // 按钮
+        Button button1 = new Button("英语", (ClickHandler) clickEvent -> speak("en-US"));
+        Button button2 = new Button("汉语", (ClickHandler) clickEvent -> speak("zh-CN"));
+        Button button3 = new Button("日语", (ClickHandler) clickEvent -> speak(""));
+        Button button4 = new Button("韩语", (ClickHandler) clickEvent -> speak(""));
+        Button button5 = new Button("俄语", (ClickHandler) clickEvent -> speak(""));
+        Button button6 = new Button("法语", (ClickHandler) clickEvent -> speak("test"));
+        Button button7 = new Button("多语种混合", (ClickHandler) clickEvent -> speak("mixin"));
+
+        // 水平面板
+        HorizontalPanel hPanel = new HorizontalPanel();
+        hPanel.setSpacing(10);
+        hPanel.add(button1);
+        hPanel.add(button2);
+        hPanel.add(button3);
+        hPanel.add(button4);
+        hPanel.add(button5);
+        hPanel.add(button6);
+        hPanel.add(button7);
+
+        // 垂直面板
+        VerticalPanel vPanel = new VerticalPanel();
+        vPanel.add(title);
+        vPanel.add(hPanel);
+
+        RootPanel.get().add(vPanel);
     }
 
-    public void speechSynthesis() {
-        // 判断浏览器是否支持语音合成
-        if (!window.object().has("speechSynthesis")) {
-            window.alert("no support for speech synthesis");
+    /**
+     * 播报英语
+     */
+    private void speak(String language) {
+        if (!checkBrowser()) {
             return;
         }
-
-        // 判断浏览器语音合成是否加载中
-        if (window.getSpeechSynthesis().isPending()) {
-            window.getSpeechSynthesis().cancel();
-        }
-
-
         getVoicesReady().then(x -> {
-            intro();
+            switch (language) {
+                case "en-US":
+                    speakEnglish();
+                    break;
+                case "zh-CN":
+                    speakChinese();
+                    break;
+                case "mixin":
+                    speakMixin();
+                    break;
+                default:
+                    window.alert("unknown language or no support for language");
+                    break;
+            }
             return null;
         }).catch_(error -> {
             window.alert(((Error) error).getMessage());
@@ -53,6 +90,114 @@ public class App implements EntryPoint {
         });
     }
 
+    /**
+     * 讲英语
+     */
+    private void speakEnglish() {
+        // 获取 Google US English 语音
+        SpeechSynthesisVoice usVoice = getVoice("en-US", voice -> true);
+        // 语音播报
+        say("Hello. Welcome to the speech synthesis demo.", usVoice);
+
+        // 获取 Google UK English Female 语音
+        SpeechSynthesisVoice femaleUKVoice = getVoice("en-GB", voice -> voice.getName().contains("Female"));
+        // 语音播报
+        say("You sound like a robot. I think I sound better.", femaleUKVoice);
+
+        // 获取 Google UK English Male 语音
+        SpeechSynthesisVoice maleUKVoice = getVoice("en-GB", voice -> voice.getName().contains("Male"));
+        // 语音播报
+        say("Actually, everything sounds better with a British accent, don't you think?", maleUKVoice);
+    }
+
+    /**
+     * 讲汉语
+     */
+    private void speakChinese() {
+        // 获取 Microsoft Huihui 语音
+        SpeechSynthesisVoice microsoftHuihuiVoice = getVoice("zh-CN", voice -> voice.getName().contains("Microsoft Huihui"));
+        // 语音播报
+        say("你好，我是微软慧慧，我可以说中文，请听：您有新短消息，请注意查收", microsoftHuihuiVoice);
+
+        // 获取 Google 普通话语音
+        SpeechSynthesisVoice googleVoice = getVoice("zh-CN", voice -> voice.getName().contains("Google 普通话"));
+        // 语音播报
+        say("你好，我是Google普通话，我也可以说中文，请听：您有新短消息，请注意查收", googleVoice);
+    }
+
+    /**
+     * 混合语音
+     */
+    private void speakMixin() {
+        boolean otherLanguages = say("en-GB", "Actually, everything sounds better with a British accent, don't you think?");
+        otherLanguages = otherLanguages | say("fr", "Sur Chrome, nous pouvons parler de nombreuses langues.");
+        otherLanguages = otherLanguages | say("es-US", "Como el español por ejemplo.");
+        otherLanguages = otherLanguages | say("es-ES", "O el típico español de España.", "Yo soy de Zaragoza!");
+        say("es-US", "Que dices mujer?", "Que mi español no es típico?", "Yo soy de Monterrey, Mejico!");
+        otherLanguages = otherLanguages | say("it", "Non dimenticare di me! parlo italiano");
+        otherLanguages = otherLanguages | say("de", "Und ich spreche deutsch.");
+        otherLanguages = otherLanguages | say("zh", "我可以说中文. 您有新短消息，请注意查收");
+        otherLanguages = otherLanguages | say("ko", "나는 한국어를 할 수 있습니다.");
+        otherLanguages = otherLanguages | say("ru", "И я говорю по-русски.");
+        say("fr", "I can even speak English with a French accent.", "C'est amusant, ça!");
+        say("fr", "Il y a aussi d'autres langues, mais ça devient un peu ennuyeux...");
+        say("pt", "Ei, espere um pouco, você conseguiu ignorar o português?", " Braaaasil!", "Braaaasil!", "Braaaasil!");
+        say("en-GB", "I want to hear other languages!");
+    }
+
+    /**
+     * 获取语音
+     *
+     * @param language  语言/区域
+     * @param predicate 过滤条件，用于筛选语音
+     * @return SpeechSynthesisVoice
+     */
+    private SpeechSynthesisVoice getVoice(String language, Predicate<SpeechSynthesisVoice> predicate) {
+        SpeechSynthesisVoice[] candidates = Arrays.stream(window.getSpeechSynthesis().getVoices())
+            .filter(voice -> voice.getLang().contains(language))
+            .toArray(SpeechSynthesisVoice[]::new);
+        if (candidates.length != 0) {
+            return Arrays.stream(candidates).filter(predicate).findFirst().orElse(candidates[0]);
+        }
+        return null;
+    }
+
+    /**
+     * 使用指定的语音播报文字
+     *
+     * @param text  文本
+     * @param voice 语音
+     */
+    private void say(String text, SpeechSynthesisVoice voice) {
+        SpeechSynthesisUtterance utterance = new SpeechSynthesisUtterance(text);
+        utterance.setVoice(voice);
+        window.getSpeechSynthesis().speak(utterance);
+    }
+
+    /**
+     * 使用指定的语言播报文字
+     *
+     * @param language 语言/区域
+     * @param text     文本
+     * @return 是否成功
+     */
+    private boolean say(String language, String... text) {
+        if (isLanguageSupported(language)) {
+            for (String phrase : text) {
+                SpeechSynthesisUtterance utterance = new SpeechSynthesisUtterance(phrase);
+                utterance.setLang(language);
+                window.getSpeechSynthesis().speak(utterance);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 准备就绪
+     *
+     * @return Promise
+     */
     private Promise<Void> getVoicesReady() {
         return new Promise<>((resolve, reject) -> {
             SpeechSynthesisVoice[] voices = window.getSpeechSynthesis().getVoices();
@@ -74,78 +219,33 @@ public class App implements EntryPoint {
         });
     }
 
-    private void intro() {
-        SpeechSynthesisVoice maleUSVoice = getPreferredVoice("en-US",
-            voice -> !voice.getName().contains("Zira")
-                && !voice.getName().contains("Female"));
-        SpeechSynthesisVoice femaleUSVoice = getPreferredVoice("en-US",
-            voice -> voice.getName().contains("Zira")
-                || voice.getName().contains("Female"));
-        SpeechSynthesisVoice maleUkVoice = getPreferredVoice("en-GB",
-            voice -> !voice.getName().contains("Female"));
-
-        say("Hello. Welcome to the speech synthesis demo.", maleUSVoice);
-        say("You sound like a robot. I think I sound better.", femaleUSVoice);
-        boolean otherLanguages =
-            say("en-GB", "Actually, everything sounds better with a British accent, don't you think?");
-        otherLanguages = otherLanguages | say("fr", "Sur Chrome, nous pouvons parler de nombreuses langues.");
-        otherLanguages = otherLanguages | say("es-US", "Como el español por ejemplo.");
-        otherLanguages = otherLanguages | say("es-ES", "O el típico español de España.", "Yo soy de Zaragoza!");
-        say("es-US", "Que dices mujer?", "Que mi español no es típico?", "Yo soy de Monterrey, Mejico!");
-        otherLanguages = otherLanguages | say("it", "Non dimenticare di me! parlo italiano");
-        otherLanguages = otherLanguages | say("de", "Und ich spreche deutsch.");
-        otherLanguages = otherLanguages | say("zh", "我可以说中文.");
-        otherLanguages = otherLanguages | say("ko", "나는 한국어를 할 수 있습니다.");
-        otherLanguages = otherLanguages | say("ru", "И я говорю по-русски.");
-        say("fr", "I can even speak English with a French accent.", "C'est amusant, ça!");
-        say("fr", "Il y a aussi d'autres langues, mais ça devient un peu ennuyeux...");
-        say("pt", "Ei, espere um pouco, você conseguiu ignorar o português?", " Braaaasil!", "Braaaasil!", "Braaaasil!");
-        if (maleUkVoice != null) {
-            say("I want to hear other languages!", maleUkVoice);
-        } else {
-            say("en-GB", "I want to hear other languages!");
+    /**
+     * 校验浏览器是否支持语音合成技术
+     *
+     * @return true or false
+     */
+    private boolean checkBrowser() {
+        // 判断浏览器是否支持语音合成
+        if (!window.object().has("speechSynthesis")) {
+            window.alert("no support for speech synthesis");
+            return false;
         }
-        if (!otherLanguages) {
-            say("Guess what, we can only speak American English.", maleUSVoice);
-            say("How dumb! I want to be on Chrome. They speak many languages there.", femaleUSVoice);
-        } else {
-            say("I'm sorry Dave. I'm afraid I can't do that.", maleUSVoice);
+        // 判断浏览器语音合成是否加载中
+        if (window.getSpeechSynthesis().isPending()) {
+            window.getSpeechSynthesis().cancel();
+            return false;
         }
+        return true;
     }
 
-    private SpeechSynthesisVoice getPreferredVoice(String language, Predicate<SpeechSynthesisVoice> predicate) {
-        SpeechSynthesisVoice[] candidates = Arrays.stream(window.getSpeechSynthesis().getVoices())
-            .filter(voice -> voice.getLang().contains(language))
-            .toArray(SpeechSynthesisVoice[]::new);
-        if (candidates.length != 0) {
-            return Arrays.stream(candidates)
-                .filter(predicate)
-                .findFirst()
-                .orElse(candidates[0]);
-        }
-        return null;
-    }
-
-    private boolean say(String language, String... text) {
-        if (isLanguageSupported(language)) {
-            for (String phrase : text) {
-                SpeechSynthesisUtterance utterance = new SpeechSynthesisUtterance(phrase);
-                utterance.setLang(language);
-                window.getSpeechSynthesis().speak(utterance);
-            }
-            return true;
-        }
-        return false;
-    }
-
+    /**
+     * 校验是否支持指定的语言
+     *
+     * @param language 语言/区域
+     * @return true or false
+     */
     private boolean isLanguageSupported(String language) {
         return Arrays.stream(window.getSpeechSynthesis().getVoices())
             .anyMatch(voice -> voice.getLang().contains(language));
-    }
-
-    private void say(String text, SpeechSynthesisVoice voice) {
-        SpeechSynthesisUtterance utterance = new SpeechSynthesisUtterance(text);
-        utterance.setVoice(voice);
-        window.getSpeechSynthesis().speak(utterance);
     }
 }
