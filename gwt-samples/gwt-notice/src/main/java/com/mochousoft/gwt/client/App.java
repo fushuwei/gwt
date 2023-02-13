16add9a784831f4c8f066ededc7f13389438b4a1
fushuwei <331044761@qq.com>
2023-02-13 23:29:59 +0800

    修改demo

--- gwt-samples/gwt-notice/src/main/java/com/mochousoft/gwt/client/App.java
+++ gwt-samples/gwt-notice/src/main/java/com/mochousoft/gwt/client/App.java
@@ -6,9 +6,14 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import gwt.jelement.notifications.Notification;
+import gwt.jelement.notifications.NotificationOptions;
 
 import java.util.logging.Logger;
+
+import static gwt.jelement.Browser.window;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -26,12 +31,18 @@ public class App implements EntryPoint {
         // 标题
         HTML title = new HTML("<h1>浏览器消息通知</h1><span>注意：需要设置浏览器允许接收来自该站点的消息，否则无法模拟效果</span>");
 
+        // 文本框
+        TextBox textBox = new TextBox();
+        textBox.setWidth("500px");
+        textBox.setValue("请输入您要通知的内容");
+
         // 按钮
-        Button button = new Button("发送通知", (ClickHandler) clickEvent -> notice());
+        Button button = new Button("发送通知", (ClickHandler) clickEvent -> notice(textBox.getValue()));
 
         // 水平面板
         HorizontalPanel hPanel = new HorizontalPanel();
         hPanel.setSpacing(10);
+        hPanel.add(textBox);
         hPanel.add(button);
 
         // 垂直面板
@@ -45,7 +56,24 @@ public class App implements EntryPoint {
     /**
      * 发送通知
      */
-    private void notice() {
+    private void notice(final String body) {
+        // 校验浏览器是否支持发送通知
+        if (!window.object().has("Notification")) {
+            window.alert("Notifications are not supported on this browser");
+            return;
+        }
 
+        // 请求浏览器授权发送通知
+        Notification.requestPermission().then(response -> {
+            if ("granted".equals(response)) {
+                NotificationOptions notificationOptions = new NotificationOptions();
+                notificationOptions.setBody(body);
+                notificationOptions.setIcon("images/gwt-logo.png");
+                notificationOptions.setImage("images/code.png");
+                notificationOptions.setRequireInteraction(true);
+                new Notification("提示", notificationOptions);
+            }
+            return null;
+        });
     }
 }
