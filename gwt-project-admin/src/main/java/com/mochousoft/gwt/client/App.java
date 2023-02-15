7c2935142487b642e35fa3b14778652ee1646ddb
fushuwei <331044761@qq.com>
2023-02-16 01:15:32 +0800

    创建模块

---
+++ gwt-project-admin/src/main/java/com/mochousoft/gwt/client/App.java
@@ -0,0 +1,21 @@
+package com.mochousoft.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+import java.util.logging.Logger;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ *
+ * @author fushuwei
+ */
+public class App implements EntryPoint {
+
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+    }
+}
