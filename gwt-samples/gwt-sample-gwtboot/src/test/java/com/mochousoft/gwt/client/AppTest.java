b222d58a98dde53fc7ced5e9def681eaedac0dcc
fushuwei <331044761@qq.com>
2023-02-18 21:56:28 +0800

    创建gwtboot演示示例

---
+++ gwt-samples/gwt-sample-gwtboot/src/test/java/com/mochousoft/gwt/client/AppTest.java
@@ -0,0 +1,22 @@
+package com.mochousoft.gwt.client;
+
+import static org.junit.Assert.assertTrue;
+
+import com.google.gwtmockito.GwtMockitoTestRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * GWT Boot Test
+ *
+ * @author fushuwei
+ */
+@RunWith(GwtMockitoTestRunner.class)
+public class AppTest {
+
+    @Test
+    public void test_ok() {
+        assertTrue(true);
+    }
+}
