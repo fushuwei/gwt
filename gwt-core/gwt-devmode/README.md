b4d73f3074f5906c456a423e2550a44bfcd7d780
fushuwei <331044761@qq.com>
2023-02-21 15:17:20 +0800

    创建gwt-devmode示例

---
+++ gwt-core/gwt-devmode/README.md
@@ -0,0 +1,28 @@
+# Build Project
+
+`cd PROJECT_ROOT_DIR`
+
+`mvn clean package`
+
+# Run Project
+
+`cd PROJECT_ROOT_DIR`
+
+`mvn gwt:generate-module gwt:devmode`
+
+## Browse Page
+
+`http://localhost:8888`
+
+# Debugging GWT in IDEA
+
+1. Edit Configurations -> JavaScript Debug -> URL
+
+`http://localhost:8888/app` *OR* `http://localhost:63342/gwt/gwt-samples/gwt-sample-gwtboot/target/gwt/devmode/war/app/index.html`
+
+2. Allow unsigned requests (Optional, Must be configured only when first step config url as http://<span></span>localhost:63342/...)
+
+`Settings -> Build, Execution, Deployment -> Debugger -> Allow unsigned requests`
+
+3. Config local-remote-mapping (Optional),
+   Reference: [Intellij 2018.2 Debugging GWT Apps](https://github.com/GwtMaterialDesign/gwt-material/wiki/Intelij-2018.2-Debugging-GWT-Apps)
---
+++ gwt-core/gwt-devmode/src/main/java/com/mochousoft/gwt/client/App.java
@@ -0,0 +1,34 @@
+package com.mochousoft.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.mochousoft.gwt.shared.People;
+
+import java.util.logging.Logger;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ *
+ * @author fushuwei
+ */
+public class App implements EntryPoint {
+
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+
+        Button button = new Button("Hello World", (ClickHandler) clickEvent -> {
+            People people = new People("Jack", 18);
+            logger.info("My name is " + people.getName() + ", and I am " + people.getAge());
+            Window.alert("View browser console.");
+        });
+
+        RootPanel.get().add(button);
+    }
+}
